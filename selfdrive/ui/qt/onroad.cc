627b984955fe8641b2c00398afbb20bb2156347a
hikee9123 <hikee9123@gmail.com>
2021-12-12 18:28:27 +0900

    test

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -267,15 +267,18 @@ void OnroadHud::drawCurrentSpeed(QPainter &p, int x, int y)
   int  brakePress = m_nBrakeStatus & 0x01;
   int  brakeLights = m_nBrakeStatus & 0x02;
 
+  m_gasVal += 0.1;
 
+  if( m_gasVal > 2 )
+    m_gasVal = 0;
 
   if( brakePress  ) val_color = QColor(255, 0, 0, 255);
   else if( brakeLights ) val_color = QColor(201, 34, 49, 100);
   else if( m_gasVal > 0 ) 
   {
-    int  gasVal = m_gasVal * 10 + 50;
+    int  gasVal = m_gasVal * 100 + 100;
     if( gasVal > 255 ) gasVal = 255;
-    val_color = QColor(200, 200, 0, gasVal);
+    val_color = QColor(255, 255, 0, gasVal);
   }
 
   configFont(p, "Open Sans", 250, "Bold");
