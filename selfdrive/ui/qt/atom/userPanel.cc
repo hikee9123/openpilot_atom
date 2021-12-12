a93df65ae1844ba44c32a17c2cb02ab819be7f8e
hikee9123 <hikee9123@gmail.com>
2021-12-12 21:34:21 +0900

    test

--- selfdrive/ui/qt/atom/userPanel.cc
+++ selfdrive/ui/qt/atom/userPanel.cc
@@ -744,7 +744,7 @@ CarSelectCombo::CarSelectCombo() : AbstractControl("Car", "자동차 모델을 �
     border: 1px solid #1e1e1e;
     border-radius: 5;
     padding: 1px 0px 1px 5px; 
-    width: 1000px;
+    QComboBox { max-width: 1000px; min-height: 40px;}
   )");
 
 
@@ -779,6 +779,7 @@ CarSelectCombo::CarSelectCombo() : AbstractControl("Car", "자동차 모델을 �
     combobox.addItem("GENESIS G80 2017");
     combobox.addItem("GENESIS G90 2017");
     combobox.setFixedWidth(700);
+    combobox.setFixedWidth(1000)
 
   hlayout->addWidget(&combobox);
 
